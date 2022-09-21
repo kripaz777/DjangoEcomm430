655473bcab581bf7267bc7c4d20a18c2bb00df7f
Aayush <aceraayush@gmail.com>
2022-09-21 17:32:45 +0545

    base.html

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from .models import *
 # Create your views here.
+class BaseView(View):
+    my_view = {}
+
+class HomeView(BaseView):
+    def get(self,request):
+
+        return render(request,'index.html',self.my_view)
