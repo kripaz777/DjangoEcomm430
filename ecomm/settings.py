f0b0762b0ca704448df1dc0bb21affa9ac7b13e3
Aayush <aceraayush@gmail.com>
2022-09-22 17:35:20 +0545

    home page

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -109,11 +109,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+import os
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- home/views.py
+++ home/views.py
@@ -4,8 +4,14 @@ from .models import *
 # Create your views here.
 class BaseView(View):
     my_view = {}
+    my_view['categories'] = Category.objects.all()
+    my_view['brands'] = Brand.objects.all()
 
 class HomeView(BaseView):
     def get(self,request):
-
+        self.my_view['sliders'] = Slider.objects.all()
+        self.my_view['ads'] = Ad.objects.all()
+        self.my_view['hots'] = Product.objects.filter(labels = 'hot')
+        self.my_view['sales'] = Product.objects.filter(labels='sale')
+        self.my_view['news'] = Product.objects.filter(labels='new')
         return render(request,'index.html',self.my_view)
