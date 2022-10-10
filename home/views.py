d83bd1eee9297c11c67bcc6ca7a408b76df99cf7
Aayush <aceraayush@gmail.com>
2022-10-10 17:33:27 +0545

    category and brand page

--- home/views.py
+++ home/views.py
@@ -6,18 +6,28 @@ class BaseView(View):
     my_view = {}
     my_view['categories'] = Category.objects.all()
     my_view['brands'] = Brand.objects.all()
+    my_view['sales'] = Product.objects.filter(labels='sale')
 
 class HomeView(BaseView):
     def get(self,request):
+        self.my_view
         self.my_view['sliders'] = Slider.objects.all()
         self.my_view['ads'] = Ad.objects.all()
         self.my_view['hots'] = Product.objects.filter(labels = 'hot')
-        self.my_view['sales'] = Product.objects.filter(labels='sale')
         self.my_view['news'] = Product.objects.filter(labels='new')
         return render(request,'index.html',self.my_view)
 
 class CategoryView(BaseView):
     def get(self,request,slug):
+        self.my_view
         ids = Category.objects.get(slug = slug).id
         self.my_view['catproducts'] = Product.objects.filter(category_id = ids)
         return render(request, 'category.html', self.my_view)
+
+
+class BrandView(BaseView):
+    def get(self,request,slug):
+        self.my_view
+        ids = Brand.objects.get(slug = slug).id
+        self.my_view['brandroducts'] = Product.objects.filter(brand_id = ids)
+        return render(request, 'brand.html', self.my_view)
